52ee8d0f023fff4a63516ff380c512307f789aec
Fernando Venturini <fernandoventurini9@hotmail.com>
2024-10-29 21:10:41 +0000

    added node and mysql

--- node/app.js
+++ node/app.js
@@ -4,7 +4,24 @@ const app = express();
 
 app.use(express.json());
 
+/* MIDDLEWARES: executado antes de qualquer rota
+app.use((req, res, next) => {
+    console.log("Acessou o Middlewares!");
+    next();
+})
+*/
+function valContato(req, res, next) {
+    if (!req.body.email) {
+        return res.json({ 
+            error: true,
+            mensagem: "Email é obrigatório!"
+        });
+    };
+    return next();
+};
+
 app.get("/", (req, res) => {
+    console.log("Acessou a rota listar!");
     res.send("Bem vindo, Fernando, Celke e Cesar!!!");
 });
 
@@ -34,8 +51,9 @@ app.get("/contatos/:id", (req, res) => {
     });
 });
 
-// Exemplo 01:
-app.post("/contato", (req, res) => {
+// Exemplo 01: REQUISICAO POST: rota cadastrar
+app.post("/contato", valContato, (req, res) => {
+    console.log("Acessou a rota cadastrar!");
     var nome = req.body.nome;
     var email = req.body.email;
     return res.json({
@@ -44,7 +62,7 @@ app.post("/contato", (req, res) => {
     });
 });
 
-// Exemplo 02:
+// Exemplo 02:REQUISICAO POST
 app.post("/contato", (req, res) => {
     var { nome } = req.body;
     var { email } = req.body;
@@ -54,7 +72,7 @@ app.post("/contato", (req, res) => {
     });
 });
 
-// METHODO PUT(Editar):
+// REQUISICAO PUT(Editar):
 app.put("/contato/:id", (req, res) => {
     const { id } = req.params;
     const { _id, nome, email } = req.body;
@@ -63,8 +81,18 @@ app.put("/contato/:id", (req, res) => {
         _id,
         nome, 
         email
+    });
+});
+
+//REQUISICAO DELETE(apagar)
+app.delete("/contato/:id", (req, res) => {
+    const { id } = req.params;
+
+    return res.json({
+        id
     })
 })
+
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080: http://localhost:8080/");
